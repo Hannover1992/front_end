e34e2db5fbc5c562e914f7216b582166afc4a0aa
PatrykKrzyzanski <Hannover1992@googlemail.com>
2022-10-18 15:10:00 +0200

    importing HTTP client Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import {NgModule, OnInit} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
 import { ProjectComponent } from './project/project.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,9 +12,13 @@ import { ProjectComponent } from './project/project.component';
     ProjectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
